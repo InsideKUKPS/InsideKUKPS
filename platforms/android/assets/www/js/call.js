2bb49ee55320183207f149d21d9746313c3f3aad
Sitthinon Chanaritthichai <31182493+FighttoSitthinon@users.noreply.github.com>
2018-04-09 20:40:17 +0700

    commit ไปให้หมดว้อยยย

---
+++ platforms/android/assets/www/js/call.js
@@ -0,0 +1,12 @@
+function goBack() {
+    window.history.back();
+  }
+  function onSuccess(result){
+    console.log("Success:"+result);
+  }
+   
+  function onError(result) {
+    console.log("Error:"+result);
+  }
+ 
+ 
